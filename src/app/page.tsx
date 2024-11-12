6e2aca8e73f690d8bd2407493d2560a53f5a493c
comiksans <59838598+comiksans@users.noreply.github.com>
2024-11-11 23:31:56 -0500

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+
+
+import { Footer } from '@/components/Footer'
+import Hero from '@/components/Hero'
+import Waitlist from '@/components/Waitlist'
+import FAQ from '@/components/FAQ'
+import Team from '@/components/Team'
+import Testimonials from '@/components/Testimonials'
+import Stats from '@/components/Stats'
+import { HowWork } from '@/components/HowWork'
+import About from '@/components/About'
+import BentoGrid from '@/components/BentoGrid'
+import About2 from '@/components/About2'
+import About3 from '@/components/About3'
+import Test from '@/components/test'
+
+
+export default function Home() {
+  return (
+    <>
+      <main>
+
+        <Hero />
+
+        <HowWork />
+        <Waitlist />
+
+        <About />
+        <About2 />
+        <About3 />
+
+        {/* <Stats /> */}
+
+        <BentoGrid />
+        <FAQ />
+
+      </main>
+      <Footer />
+    </>
+  )
+}
